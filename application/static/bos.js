6ee4fcbbbd59934d938e80a30d40b699b54d0f2c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-04-26 10:28:00 +0300

    Client application stub

---
+++ application/static/bos.js
@@ -0,0 +1,26 @@
+import { Metacom } from './metacom.js';
+
+class Application {
+  constructor() {
+    const protocol = location.protocol === 'http:' ? 'ws' : 'wss';
+    this.metacom = Metacom.create(`${protocol}://${location.host}/api`);
+  }
+}
+
+window.addEventListener('load', async () => {
+  window.application = new Application();
+  window.api = window.application.metacom.api;
+  await application.metacom.load('auth', 'bos', 'store');
+  const token = localStorage.getItem('metarhia.session.token');
+  let logged = false;
+  if (token) {
+    const res = await api.auth.restore({ token });
+    logged = res.status === 'logged';
+  }
+  if (!logged) {
+    const res = await api.auth.signin({ login: 'marcus', password: 'marcus' });
+    if (res.token) {
+      localStorage.setItem('metarhia.session.token', res.token);
+    }
+  }
+});
