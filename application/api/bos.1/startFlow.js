feabb0ae5a5eb8a033988957783ab09c82673721
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-04-29 01:16:33 +0300

    Client-side form processing stub

--- application/api/bos.1/startFlow.js
+++ application/api/bos.1/startFlow.js
@@ -27,36 +27,17 @@
     });
 
     flow.on('form/show', (step) => {
-      console.log({ model });
       const form = model.entities.get('OrderForm').fields;
       context.client.emit('bos/form', { step, form });
       console.log({ 'form/show': step });
-
-      let data = {};
-      if (step.command === 'Form `Order`') {
-        data = {
-          product: 'Motorola Edge 20 Pro',
-          carrier: 'Postal service',
-          amount: 2,
-        };
-      }
-      if (step.command === 'Form `Payment`') {
-        data = {
-          amount: 20000,
-        };
-      }
-      flow.emit('form/submit', data);
     });
 
-    await flow.exec(name).catch((err) => {
+    flow.exec(name).catch((err) => {
       console.error(err);
       context.client.emit('flows/error', err);
     });
 
-    // set is not a function
-    // Runtime.prototype.processes = {} -> lowscript/runtime.js
-    // domain.runtime.processes.set(context.client, flow);
-    console.dir({ processes: domain.runtime.processes });
+    domain.runtime.flows.set(context.client, flow);
     domain.runtime.clients.set(flow, context.client);
 
     return { success: true };
